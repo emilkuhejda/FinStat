46d949a911a839444ea600e95b439e76aa35c78a
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-09-07 11:43:14 +0200

    Fix static code analysis.

--- FinStat.Mobile/FinStat.Mobile/GlobalSuppressions.cs
+++ FinStat.Mobile/FinStat.Mobile/GlobalSuppressions.cs
@@ -6,5 +6,3 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "By design", Scope = "member", Target = "~M:FinStat.Mobile.ViewModels.ViewModelBase.Dispose(System.Boolean)")]
-[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "It is disposed with caller", Scope = "member", Target = "~M:FinStat.Mobile.Utils.ThreadHelper.InvokeOnUiThread(System.Action)")]
-[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "It is disposed with caller", Scope = "member", Target = "~M:FinStat.Mobile.Utils.ThreadHelper.InvokeOnUiThread``1(System.Func{``0})~``0")]
