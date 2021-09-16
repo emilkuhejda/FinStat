8a72adab81b24009f72d0cd8cfba41eed61c6348
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-09-16 09:49:28 +0200

    Code refactoring.

---
+++ FinStat.Mobile/FinStat.Mobile/ViewModels/DataGrid/ParameterDefinition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FinStat.Domain.Enums;
+using FinStat.Domain.Models;
+
+namespace FinStat.Mobile.ViewModels.DataGrid
+{
+    public class ParameterDefinition
+    {
+        public ParameterDefinition(string title, Func<IncomeStatement, DisplayUnit, string> value)
+        {
+            Title = title;
+            Value = value;
+        }
+
+        public string Title { get; }
+
+        public Func<IncomeStatement, DisplayUnit, string> Value { get; }
+    }
+}
