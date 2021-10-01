3a6d0901667d635e5db29d1cf38a972f930b187a
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-10-01 10:54:59 +0200

    Create price chart page.

---
+++ FinStat.Mobile/FinStat.Mobile/ViewModels/PriceChartPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using FinStat.Common.Utils;
+using FinStat.Domain.Models;
+using FinStat.Mobile.Extensions;
+using FinStat.Resources.Localization;
+using Prism.Navigation;
+
+namespace FinStat.Mobile.ViewModels
+{
+    public class PriceChartPageViewModel : ViewModelBase
+    {
+        public PriceChartPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            CanGoBack = true;
+            HasTitleBar = true;
+            Title = Loc.Text(TranslationKeys.NoAvailableData);
+        }
+
+        public bool HasDataToPlot => false;
+
+        protected override async Task LoadDataAsync(INavigationParameters navigationParameters)
+        {
+            using (new OperationMonitor(OperationScope))
+            {
+                var searchResult = navigationParameters.GetValue<SearchResult>();
+                if (searchResult == null)
+                    return;
+
+                Title = searchResult.Symbol;
+
+                await Task.CompletedTask;
+            }
+        }
+    }
+}
